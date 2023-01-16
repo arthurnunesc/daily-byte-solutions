bcdbee0d5f42defb831c660694271a0dc264b413
Arthur Nunes <arthurnunesc@proton.me>
2023-01-16 16:20:25 +0100

    First day done

---
+++ week-one/day1-reverse_string.py
@@ -0,0 +1,15 @@
+def reverse_string_easy(string):
+    return string[::-1]
+
+def reverse_string_manually(string):
+    string_as_a_list = list(string)
+    reversed_string_as_a_list = []
+    for i in range(len(string_as_a_list)):
+        reversed_string_as_a_list.append(string_as_a_list[-1])
+        string_as_a_list.pop(-1)
+
+    return "".join(reversed_string_as_a_list)
+
+
+print(reverse_string_easy("PPk"))
+print(reverse_string_manually("PeNis"))
